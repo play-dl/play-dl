1c8c3cebd50fc6bb75a103a49a6976d566ca509b
killer069 <65385476+killer069@users.noreply.github.com>
2021-08-13 21:56:44 +0530

    Stream first then spotify

--- node-youtube-dl/Spotify/index.ts
+++ node-youtube-dl/Spotify/index.ts
@@ -1,1 +1,4 @@
 //Spotify Work
+import stream from 'stream'
+
+console.log(new stream.PassThrough())
--- node-youtube-dl/index.ts
+++ node-youtube-dl/index.ts
@@ -1,12 +1,14 @@
 //This File is in testing stage, everything will change in this
 import { playlist_info, video_basic_info, video_info, search } from "./YouTube";
 
-let main = async() => {
-    let time_start = Date.now()
-    let result = await search('Rick Roll')
-    console.log(result[0].url)
-    let time_end = Date.now()
-    console.log(`Time Taken : ${(time_end - time_start)/1000} seconds`)
-}
+// let main = async() => {
+//     let time_start = Date.now()
+//     let result = await search('Rick Roll')
+//     console.log(result[0].url)
+//     let time_end = Date.now()
+//     console.log(`Time Taken : ${(time_end - time_start)/1000} seconds`)
+// }
 
-main()
+// main()
+
+export var youtube = { playlist_info, video_basic_info, video_info, search }
