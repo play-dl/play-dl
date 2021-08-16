97b1eed267ba7b1967d903fa5a500ec1ca902f39
killer069 <65385476+killer069@users.noreply.github.com>
2021-08-16 15:19:31 +0530

    Huge Update

---
+++ play-dl/index.ts
@@ -0,0 +1,5 @@
+//This File is in testing stage, everything will change in this
+import { playlist_info, video_basic_info, video_info, search, stream } from "./YouTube";
+
+
+export let youtube = { playlist_info, video_basic_info, video_info, search, stream }
