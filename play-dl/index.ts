a15555d3511874fb973a21fb114ccce64cd40324
killer069 <65385476+killer069@users.noreply.github.com>
2021-09-01 11:22:44 +0530

    Wrong Update

--- play-dl/index.ts
+++ play-dl/index.ts
@@ -1,7 +1,1 @@
 export { playlist_info, video_basic_info, video_info, search, stream, stream_from_info, validate, validate_playlist } from "./YouTube";
-
-import { video_basic_info } from '.'
-
-(async() => {
-    let vid = await video_basic_info('https://www.youtube.com/watch?v=ALZHF5UqnU4')
-})()
