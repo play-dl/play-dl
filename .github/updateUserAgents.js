c757fa064b998c68a652e7180d0fa489e7cda255
absidue <48293849+absidue@users.noreply.github.com>
2022-02-09 18:34:35 +0100

    Add workflow to update the user agents once a month

---
+++ .github/updateUserAgents.js
@@ -0,0 +1,19 @@
+const { writeFileSync } = require('fs');
+const UserAgent = require('user-agents');
+
+const generator = new UserAgent({ deviceCategory: 'desktop' });
+const userAgents = [];
+
+for (let i = 0; i < 10; i++) {
+	let userAgent = generator.random();
+
+	// only use Windows and Linux user agents and exclude Internet Explorer ones
+	while (!(userAgent.data.platform.startsWith('Win') || userAgent.data.platform.startsWith('Linux'))
+		|| userAgent.data.userAgent.includes('; MSIE') || userAgent.data.userAgent.includes('Trident/')) {
+		userAgent = generator.random();
+	}
+
+	userAgents.push(userAgent.toString());
+}
+
+writeFileSync('play-dl/Request/useragents.json', JSON.stringify(userAgents, null, 4));
