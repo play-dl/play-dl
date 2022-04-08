0b17b1619a8ea6ff3e24b0ffeab096c2c7db488c
Gary50613 <50613gary@gmail.com>
2022-04-08 11:49:42 +0800

    prevent null decoratedPlayerBarRenderer

--- play-dl/YouTube/utils/extractor.ts
+++ play-dl/YouTube/utils/extractor.ts
@@ -230,7 +230,7 @@ export async function video_basic_info(url: string, options: InfoOptions = {}): 
         });
     }
     const rawChapters =
-        initial_response.playerOverlays.playerOverlayRenderer.decoratedPlayerBarRenderer.decoratedPlayerBarRenderer.playerBar?.multiMarkersPlayerBarRenderer.markersMap.find(
+        initial_response.playerOverlays.playerOverlayRenderer.decoratedPlayerBarRenderer?.decoratedPlayerBarRenderer.playerBar?.multiMarkersPlayerBarRenderer.markersMap.find(
             (m: any) => m.key === 'DESCRIPTION_CHAPTERS'
         )?.value?.chapters;
     const chapters: VideoChapter[] = [];
