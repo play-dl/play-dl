60a3bf9f0bbfc26e335e6d356c8bc0fa595f87e5
kaisic1224 <hiddenshadows1224@gmail.com>
2023-07-18 05:07:20 -0400

    ^^

--- dist/Request/useragent.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function setUserAgent(array: string[]): void;
-export declare function getRandomUserAgent(): string;
-//# sourceMappingURL=useragent.d.ts.map
--- dist/YouTube/utils/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { video_basic_info, video_info, decipher_info, playlist_info, yt_validate, extractID } from './extractor';
-//# sourceMappingURL=index.d.ts.map
